8c681ba73b16055dcd5571ea529c278a80c849f4
JP Vendramini <joaopedrovendraminiv90@gmail.com>
2021-07-15 15:37:12 -0300

    MsqlConnection

---
+++ mysql-connection/index.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const mysql = require('mysql');
+const {Server} = require('socket.io');
+const io = new Server(server);
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname+'/index.html');
+});
+
+var db = mysql.createConnection({
+    host: 'compressinfo.ddns.net',
+    port: 13695,
+    user: 'sa',
+    database: 'node',
+    password:'@Compress1'    
+});
+
+db.connect((error)=>{
+    if(error) {
+        console.log(error);
+    }else{
+        console.log("Connected!!");
+    }
+});
+
+function sendNotes(note){
+    db.query('INSERT INTO notes (note) VALUES (?)', note);        
+};
+
+var notes = [];
+var isInitiated = false;
+
+io.on('connection', (socket) => {
+    console.log('User connected :)');
+    socket.on('disconnect', ()=>{
+        console.log('User disconnected!! :(');
+        isInitiated = false;
+    });    
+    
+    socket.on('chat message', (msg)=>{
+        sendNotes(msg);
+        if(!isInitiated){
+            db.query('SELECT * FROM notes').on('result',(data)=>{
+                io.emit('chat message', data.note);
+            });
+            isInitiated = true;
+        }
+        io.emit('chat message', msg);
+    });
+});
+
+
+server.listen(3000, ()=>{
+    console.log('Listening on *3000');
+});
