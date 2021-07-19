aa9337e189ad1c5530970babae86952271a70e6b
JP Vendramini <joaopedrovendraminiv90@gmail.com>
2021-07-19 16:55:08 -0300

    chat-calc

--- chat_calc/database.js
+++ chat_calc/database.js
@@ -1,4 +1,43 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const {Server} = require('socket.io');
+const io = new Server(server);
+
 const mysql = require('mysql');
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname+'/index.html');
+});
+
+isInitialized = false;
+io.on('connection',(socket)=>{    
+    console.log('User connected!');
+    socket.on('disconnect',()=>{
+        console.log('User disconnected...');
+    });    
+    if(!isInitialized){
+        db.query('SELECT * FROM user')
+        .on('result', (data)=>{                    
+            socket.emit('currentUsers', data);
+        });            
+    }
+    socket.on('getMessages',(userId)=>{
+        db.query(`SELECT message.idMessage, user.nome, message.tipo, message.content, message.time
+        FROM user
+        INNER JOIN message
+        ON user.idUser = message.idUser
+        WHERE message.idUser LIKE '%${userId}%'`).on('result',(data)=>{
+            socket.emit('messagesFromUser', data)        
+        });
+    })
+    socket.on('insertMessage',(msg)=>{
+        console.log(msg);
+    })
+});
+
+server.listen(9000, ()=>{console.log('Listening on *9000')});
 
 //DATABASE CONNECTION
 db = mysql.createConnection({
@@ -9,19 +48,18 @@ db = mysql.createConnection({
     database: 'chatcalc'    
 });
 
-exports.db = ()=>{
-    db.connect((error)=>{
-        if(error){
-            console.log(error);
-        }else{
-            console.log('DB connected ;)');
-        }
-    })
-};
+
+db.connect((error)=>{
+    if(error){
+        console.log(error);
+    }else{
+        console.log('DB connected ;)');
+    }
+})
 
 
 //INSERÇÃO E APRESENTAÇÂOD E USUÁRIOS
-exports.dbInsertUser = (nome)=>{
+dbInsertUser = (nome)=>{
     db.query(`INSERT INTO user (nome)
     SELECT * FROM(SELECT '${nome}') AS tmp
     WHERE NOT EXISTS(SELECT nome FROM user WHERE nome = '${nome}')
@@ -34,20 +72,20 @@ exports.dbInsertUser = (nome)=>{
     });
 };
 
-exports.dbGetUsers = ()=>{
-    db.query('SELECT * FROM user').on('result', (data)=>{
+dbGetUsers = ()=>{
+    db.query('SELECT * FROM user').on('result', (data)=>{        
         console.log(data);
     });
 };
 
 
 //INSERÇÃO E APRESENTAÇÃO DE MENSAGENS
-exports.debInsertMessage = (idUser, content, tipo, time, seem = false)=>{
+dbInsertMessage = (idUser, content, tipo, time, seem = false)=>{
     db.query(`INSERT INTO message(idUser, content, tipo, time, seem) 
     VALUES(${idUser},'${content}','${tipo}','${time}',${seem})`);
 };
 
-exports.dbGetMessagesFromUser = (idUser)=>{
+dbGetMessagesFromUser = (idUser)=>{
     db.query(`SELECT user.nome, message.tipo, message.content, message.time
     FROM user
     INNER JOIN message
@@ -59,7 +97,7 @@ exports.dbGetMessagesFromUser = (idUser)=>{
 
 
 //Get the last message so that the user can see It in the list of Users
-exports.dbGetLastMessage = (idUser)=>{
+dbGetLastMessage = (idUser)=>{
     db.query(`SELECT message.content, message.seem FROM message
     WHERE message.idUser = '${idUser}'
     ORDER BY message.idMessage DESC
@@ -69,7 +107,7 @@ exports.dbGetLastMessage = (idUser)=>{
 };
 
 //For notifing that there are messages not read by the client
-exports.dbUpdateLastSeem = (idUser)=>{
+dbUpdateLastSeem = (idUser)=>{
     db.query(`UPDATE message
     SET message.seem = true
     WHERE message.idUser = '${idUser}';`);
--- chat_calc/index.js
+++ chat_calc/index.js
@@ -1,23 +1,27 @@
-const db = require('./database');
-const express = require('express');
-const app = express();
-const http = require('http');
-const server = http.createServer(app);
-const {Server} = require('socket.io');
-const io = new Server(server);
+// const db = require('./database');
+// const express = require('express');
+// const app = express();
+// const http = require('http');
+// const server = http.createServer(app);
+// const {Server} = require('socket.io');
+// const io = new Server(server);
 
-db.db();
-// db.dbInsertUser('Rafael');
+// db.db();
+// // db.dbInsertUser('Rafael');
 
-app.get('/', (req, res)=>{
-    res.sendFile(__dirname+'/index.html');
-});
+// app.get('/', (req, res)=>{
+//     res.sendFile(__dirname+'/index.html');
+// });
 
-io.on('connection',(socket)=>{    
-    console.log('User connected!');
-    socket.on('disconnect',()=>{
-        console.log('User disconnected...');
-    });    
-});
+// isInitialized = false;
+// io.on('connection',(socket)=>{    
+//     console.log('User connected!');
+//     socket.on('disconnect',()=>{
+//         console.log('User disconnected...');
+//     });    
+//     if(!isInitialized){
+//         db.dbGetUsers();        
+//     }
+// });
 
-server.listen(9000, ()=>{console.log('Listening on *9000')});
+// server.listen(9000, ()=>{console.log('Listening on *9000')});
